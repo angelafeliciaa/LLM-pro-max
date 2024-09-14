80cd891edfcda87b994e8004b5287309b36aebd9
Yikachu <leon.liang.2004@gmail.com>
2024-09-14 04:08:05 -0400

    Finish elementary dependency_graph_gen functionality
    Able to scan a python file with no classes and create a dependency graph of functions

--- DependencyGraphGen/dependency_graph_gen.py
+++ DependencyGraphGen/dependency_graph_gen.py
@@ -1,12 +1,60 @@
 import inspect
 import importlib
+import re
 
 test_code = importlib.import_module("test_code")
+
+
+class Node():
+    """
+    Node that represents a function
+    """
+    def __init__(self, name) -> None:
+        self.name = name
+        self.to_nodes:list[Node] = []
+
+    def add_edge_to(self, to_node):
+        if to_node == self:
+            return
+        if to_node not in self.to_nodes:
+            self.to_nodes.append(to_node)
+
+class Graph():
+    """
+    Graph that represents a bunch of nodes (representing a whole python module)
+    """
+    def __init__(self) -> None:
+        self.nodes:list[Node] = []
+
+    def __iter__(self):
+        return iter(self.nodes)
+
+    def add_node(self, node:Node):
+        if node not in self.nodes:
+            self.nodes.append(node)
+
+    def get_node_by_name(self, name:str):
+        for n in self.nodes:
+            if n.name == name:
+                return n
+        return None
 
 fn_names = []
 for fn in dir(test_code):
     if fn[:2] != "__":
         fn_names.append(fn)
 
-getattr(test_code, fn_names[0])()
+graph = Graph()
+for fn in fn_names:
+    n = Node(fn)
+    graph.add_node(n)
 
+for n in graph:
+    source = inspect.getsource(getattr(test_code, n.name))
+    for fn in fn_names:
+        if n.name == fn:
+            continue # do not add edge to self
+        if re.search(f"{fn}(.*)", source): # if we find foo() or whatever in the source code
+            n.add_edge_to(graph.get_node_by_name(fn))
+
+print(graph)
